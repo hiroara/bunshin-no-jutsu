37177a045ee9813c54ed7f2e0d5586358c5a690d
Arai Hiroki <hiroara62@gmail.com>
2018-12-11 00:55:47 +0900

    confirm before sync

--- bunshin/cmd/sync.go
+++ bunshin/cmd/sync.go
@@ -1,9 +1,33 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func runSync() {
-	fmt.Println("YEAH SYNC")
+	if confirmSync() {
+		fmt.Println("YEAH!")
+	} else {
+		fmt.Println("OH..")
+	}
+}
+
+func confirmSync() bool {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Are you sure to sync files? (Y/n): ")
+		text, _ := reader.ReadString('\n')
+		switch strings.ToLower(strings.TrimSpace(text)) {
+		case "", "y", "yes":
+			return true
+		case "n", "no":
+			return false
+		default:
+			fmt.Println("Please input `y` or `n`.")
+			continue
+		}
+	}
 }
